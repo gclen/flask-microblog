c142015345e3d25521d8db19da6f644d52ef2f1f
gclen <graham.clendenning@uoit.net>
2017-01-24 20:47:55 -0500

    Completed part eleven

--- app/__init__.py
+++ app/__init__.py
@@ -4,11 +4,28 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_openid import OpenID
 from flask_mail import Mail
+from flask.json import JSONEncoder
+from .momentjs import momentjs
 from config import basedir, ADMINS, MAIL_SERVER, MAIL_PORT, MAIL_USERNAME, MAIL_PASSWORD
+
+
+class CustomJSONEncoder(JSONEncoder):
+    """This class adds support for lazy translation texts to Flasks
+       JSON encoder. This is necessary when flashing translated texts"""
+    def default(self, obj):
+        from speaklater import is_lazy_string
+        if is_lazy_string(obj):
+            try:
+                return unicode(obj)  # python 2
+            except NameError:
+                return str(obj)  # python 3
+        return super(CustomJSONEncoder, self).default(obj)
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
+app.jinja_env.globals['momentjs'] = momentjs
+app.json_encoder = CustomJSONEncoder
 
 # Setting up logging
 if not app.debug:
